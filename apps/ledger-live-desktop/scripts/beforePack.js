ed853f5fcfef72ab32cef20192309a74326f2595
donovan6000 <nicolasflamel@mwcwallet.com>
2023-04-23 18:55:56 -0700

    Fixed using Node native addon version of libs. Remove patching libs since it's no longer used.

--- apps/ledger-live-desktop/scripts/beforePack.js
+++ apps/ledger-live-desktop/scripts/beforePack.js
@@ -3,6 +3,19 @@ const fs = require("fs");
 const { processNativeModules } = require("native-modules-tools");
 const lldRoot = path.resolve(__dirname, "..");
 
+function copyNativeModulesToRoot(dir) {
+	for(const file of fs.readdirSync(dir, { withFileTypes: true })) {
+		if(file.isDirectory()) {
+			if(file.name !== "prebuilds") {
+				copyNativeModulesToRoot(path.join(dir, file.name));
+			}
+		}
+		else if(file.name.endsWith(".node")) {
+			fs.copyFileSync(path.join(dir, file.name), path.join(lldRoot, "dist", "node_modules", file.name));
+		}
+	}
+}
+
 exports.default = async function(context) {
   // Rebuild native modules
   await context.packager.info.installAppDependencies(context.packager.platform, context.arch);
@@ -10,4 +23,6 @@ exports.default = async function(context) {
   fs.rmSync(path.join(lldRoot, "dist", "node_modules"), { recursive: true });
   // Find native modules and copy them to ./dist/node_modules with their dependencies.
   processNativeModules({ root: lldRoot, destination: "dist", silent: true });
+  // Copy native modules to root
+  copyNativeModulesToRoot(path.join(lldRoot, "dist", "node_modules"));
 };
