e2f8d3db3a7ad083c5840b25e77f4d3820435405
donovan6000 <nicolasflamel@mwcwallet.com>
2023-04-28 08:20:06 -0700

    Linting fixes

--- libs/ledger-live-common/src/families/mimblewimble_coin/react.ts
+++ libs/ledger-live-common/src/families/mimblewimble_coin/react.ts
@@ -169,24 +169,22 @@ export const addSentTransactionToAccount = (
       freshAddresses: [freshAddress],
       freshAddress: freshAddress.address,
       freshAddressPath: freshAddress.derivationPath,
-      operations: account.operations.map(
-        (operation: Operation): Operation => {
-          if (
-            inputsSpent.indexOf(operation.id) !== -1 &&
-            !operation.extra.spent
-          ) {
-            return {
-              ...operation,
-              extra: {
-                ...operation.extra,
-                spent: true,
-              },
-            };
-          } else {
-            return operation;
-          }
+      operations: account.operations.map((operation: Operation): Operation => {
+        if (
+          inputsSpent.indexOf(operation.id) !== -1 &&
+          !operation.extra.spent
+        ) {
+          return {
+            ...operation,
+            extra: {
+              ...operation.extra,
+              spent: true,
+            },
+          };
+        } else {
+          return operation;
         }
-      ),
+      }),
       mimbleWimbleCoinResources: {
         ...(account as MimbleWimbleCoinAccount).mimbleWimbleCoinResources,
         nextIdentifier: new Identifier(Buffer.from(nextIdentifier, "hex")),
